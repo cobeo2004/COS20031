baf8435e80514104df78b92690ba91dc34e2c463
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-01 20:39:48 +1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # COS20031
+
+## This is the repositories for COS20031
+
+## Member
+
+### Xuan Tuan Minh Nguyen
+
+### Mai An Nguyen
+
+### Trong Dat Hoang
+
+### Phuc Vinh Le
+
+### Phuong Doanh Ha
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COS20031</title>
+</head>
+<body>
+    <?php
+        echo("<h1>Tokyo, saranghe bro this is new york</h1>");
+    ?>
+    <h1>Hello World</h1>
+</body>
+</html>
