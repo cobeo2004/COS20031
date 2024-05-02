19613d71a77cd3ef19811688a2fab877786a867e
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-02 10:25:54 +1000

    change dat hoang

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         echo("<h1>Tokyo, saranghe bro this is new york</h1>");
     ?>
     <h1>Toi la cho ten Dat Hoang</h1>
+    <h2>Dat Hoang la con chos</h2>
     <h1>Hello World</h1>
 </body>
 </html>
