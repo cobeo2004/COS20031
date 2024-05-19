cd381bfe3f01e575ceccdd14c54d6eb0e29922d5
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-19 23:20:28 +1000

    Commit: ClassTable Data

---
+++ Database/Mutations/ClassTable.mutate.sql
@@ -0,0 +1,18 @@
+-- Insert values into ClassTable
+INSERT INTO ClassTable (ClassID, AgeGroup, Gender) VALUES
+(1, 'Open', 'Female'),
+(2, 'Open', 'Male'),
+(3, '50+', 'Female'),
+(4, '50+', 'Male'),
+(5, '60+', 'Female'),
+(6, '60+', 'Male'),
+(7, '70+', 'Female'),
+(8, '70+', 'Male'),
+(9, 'Under 21', 'Female'),
+(10, 'Under 21', 'Male'),
+(11, 'Under 18', 'Female'),
+(12, 'Under 18', 'Male'),
+(13, 'Under 16', 'Female'),
+(14, 'Under 16', 'Male'),
+(15, 'Under 14', 'Female'),
+(16, 'Under 14', 'Male');
