182be4db73d6193551cd77d80683b73146f70380
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-22 11:04:54 +1000

    Fix: EndScoreTable Sequence

--- Database/Mutations/EndScoreTable.mutate.sql
+++ Database/Mutations/EndScoreTable.mutate.sql
@@ -1,255 +1,350 @@
 
 -- For RangeID: 1
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 3, 3, 9, 9, 9, 41, 1);
-
-
--- For RangeID: 1
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 6, 5, 3, 5, 1, 23, 1);
-
+VALUES (10, 8, 9, 9, 5, 1, 42, 1);
+            
 
 -- For RangeID: 1
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 8, 1, 2, 7, 9, 29, 1);
-
+VALUES (8, 4, 5, 7, 4, 10, 38, 1);
+            
 
 -- For RangeID: 1
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 9, 6, 7, 4, 10, 46, 1);
+VALUES (5, 4, 10, 3, 9, 9, 40, 1);
+            
 
+-- For RangeID: 1
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (6, 8, 1, 5, 7, 7, 34, 1);
+            
+
+-- For RangeID: 1
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (8, 4, 5, 7, 6, 3, 33, 1);
+            
+
+-- For RangeID: 1
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 6, 2, 7, 8, 10, 42, 1);
+            
 
 -- For RangeID: 2
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 7, 1, 7, 3, 1, 21, 2);
-
-
--- For RangeID: 2
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 8, 2, 7, 1, 7, 28, 2);
-
+VALUES (5, 1, 7, 5, 7, 2, 27, 2);
+            
 
 -- For RangeID: 2
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 9, 8, 2, 4, 5, 38, 2);
-
+VALUES (7, 7, 9, 2, 6, 1, 32, 2);
+            
 
 -- For RangeID: 2
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (7, 6, 2, 5, 3, 5, 28, 2);
+VALUES (3, 10, 6, 1, 7, 1, 28, 2);
+            
 
+-- For RangeID: 2
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 5, 8, 7, 5, 9, 43, 2);
+            
+
+-- For RangeID: 2
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (8, 2, 10, 4, 8, 7, 39, 2);
+            
+
+-- For RangeID: 2
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (4, 1, 10, 1, 8, 8, 32, 2);
+            
 
 -- For RangeID: 3
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 2, 8, 3, 6, 1, 30, 3);
-
-
--- For RangeID: 3
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 8, 3, 8, 3, 7, 32, 3);
-
+VALUES (6, 6, 6, 8, 2, 9, 37, 3);
+            
 
 -- For RangeID: 3
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (5, 5, 8, 1, 10, 2, 31, 3);
-
+VALUES (8, 7, 2, 9, 8, 8, 42, 3);
+            
 
 -- For RangeID: 3
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 2, 3, 8, 6, 7, 29, 3);
+VALUES (9, 6, 9, 7, 2, 7, 40, 3);
+            
 
+-- For RangeID: 3
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (8, 10, 4, 5, 9, 4, 40, 3);
+            
+
+-- For RangeID: 3
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (1, 9, 3, 1, 8, 7, 29, 3);
+            
+
+-- For RangeID: 3
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (4, 8, 4, 2, 9, 7, 34, 3);
+            
 
 -- For RangeID: 4
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (1, 1, 3, 5, 4, 6, 20, 4);
-
-
--- For RangeID: 4
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 9, 5, 7, 4, 2, 36, 4);
-
+VALUES (7, 4, 9, 5, 3, 2, 30, 4);
+            
 
 -- For RangeID: 4
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 5, 5, 2, 3, 4, 28, 4);
-
+VALUES (6, 2, 9, 4, 6, 6, 33, 4);
+            
 
 -- For RangeID: 4
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 4, 10, 6, 5, 5, 39, 4);
+VALUES (6, 6, 3, 6, 2, 8, 31, 4);
+            
 
+-- For RangeID: 4
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (4, 4, 3, 8, 5, 4, 28, 4);
+            
+
+-- For RangeID: 4
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 2, 3, 6, 7, 4, 31, 4);
+            
+
+-- For RangeID: 4
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (5, 4, 4, 3, 3, 4, 23, 4);
+            
 
 -- For RangeID: 5
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 4, 10, 5, 8, 9, 46, 5);
-
-
--- For RangeID: 5
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 1, 2, 5, 8, 2, 21, 5);
-
+VALUES (6, 5, 2, 3, 2, 5, 23, 5);
+            
 
 -- For RangeID: 5
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 1, 8, 6, 6, 9, 33, 5);
-
+VALUES (7, 9, 6, 1, 9, 10, 42, 5);
+            
 
 -- For RangeID: 5
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 1, 10, 5, 2, 10, 36, 5);
+VALUES (2, 9, 4, 8, 5, 2, 30, 5);
+            
 
+-- For RangeID: 5
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 2, 3, 9, 6, 8, 37, 5);
+            
+
+-- For RangeID: 5
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (5, 5, 8, 3, 7, 3, 31, 5);
+            
+
+-- For RangeID: 5
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 4, 8, 6, 9, 5, 41, 5);
+            
 
 -- For RangeID: 6
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (5, 9, 5, 5, 4, 6, 34, 6);
-
-
--- For RangeID: 6
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 1, 1, 9, 9, 9, 32, 6);
-
+VALUES (4, 1, 5, 6, 8, 6, 30, 6);
+                
 
 -- For RangeID: 6
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 4, 4, 10, 7, 7, 41, 6);
-
+VALUES (8, 2, 9, 1, 2, 2, 24, 6);
+                
 
 -- For RangeID: 6
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 4, 2, 10, 4, 1, 29, 6);
+VALUES (2, 7, 2, 3, 5, 6, 25, 6);
+                
 
+-- For RangeID: 6
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (10, 4, 6, 5, 7, 8, 40, 6);
+                
+
+-- For RangeID: 6
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 6, 9, 1, 3, 5, 33, 6);
+                
 
 -- For RangeID: 7
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 2, 10, 6, 6, 4, 36, 7);
-
-
--- For RangeID: 7
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (1, 5, 1, 9, 6, 7, 29, 7);
-
+VALUES (4, 4, 3, 5, 10, 8, 34, 7);
+                
 
 -- For RangeID: 7
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 8, 6, 8, 10, 3, 37, 7);
-
+VALUES (3, 3, 2, 5, 7, 2, 22, 7);
+                
 
 -- For RangeID: 7
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 7, 9, 6, 1, 2, 28, 7);
+VALUES (2, 10, 3, 8, 2, 7, 32, 7);
+                
 
+-- For RangeID: 7
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (8, 3, 4, 4, 5, 3, 27, 7);
+                
+
+-- For RangeID: 7
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (3, 1, 4, 7, 5, 7, 27, 7);
+                
 
 -- For RangeID: 8
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 1, 8, 5, 7, 6, 29, 8);
-
-
--- For RangeID: 8
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (7, 6, 7, 2, 8, 2, 32, 8);
-
+VALUES (8, 5, 2, 7, 4, 7, 33, 8);
+                
 
 -- For RangeID: 8
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 10, 3, 3, 5, 5, 36, 8);
-
+VALUES (3, 6, 1, 1, 6, 4, 21, 8);
+                
 
 -- For RangeID: 8
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 6, 1, 10, 9, 10, 38, 8);
+VALUES (10, 3, 4, 3, 9, 6, 35, 8);
+                
 
+-- For RangeID: 8
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (6, 2, 5, 6, 2, 1, 22, 8);
+                
+
+-- For RangeID: 8
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (10, 6, 10, 5, 9, 4, 44, 8);
+                
 
 -- For RangeID: 9
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (4, 8, 9, 4, 7, 4, 36, 9);
-
-
--- For RangeID: 9
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (7, 4, 10, 5, 9, 3, 38, 9);
-
+VALUES (10, 6, 5, 6, 3, 3, 33, 9);
+                
 
 -- For RangeID: 9
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (4, 8, 5, 1, 7, 6, 31, 9);
-
+VALUES (6, 9, 5, 2, 7, 5, 34, 9);
+                
 
 -- For RangeID: 9
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 3, 8, 8, 8, 2, 37, 9);
+VALUES (4, 2, 2, 4, 3, 8, 23, 9);
+                
 
+-- For RangeID: 9
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (6, 2, 7, 7, 4, 6, 32, 9);
+                
+
+-- For RangeID: 9
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (1, 8, 5, 7, 6, 4, 31, 9);
+                
 
 -- For RangeID: 10
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 2, 5, 7, 3, 8, 28, 10);
-
-
--- For RangeID: 10
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 2, 6, 2, 6, 5, 24, 10);
-
+VALUES (5, 6, 6, 8, 3, 5, 33, 10);
+                
 
 -- For RangeID: 10
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 3, 9, 4, 6, 8, 39, 10);
-
+VALUES (1, 3, 9, 8, 2, 5, 28, 10);
+                
 
 -- For RangeID: 10
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (6, 10, 7, 10, 2, 5, 40, 10);
+VALUES (4, 1, 9, 1, 2, 4, 21, 10);
+                
 
+-- For RangeID: 10
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 7, 4, 9, 9, 10, 48, 10);
+                
+
+-- For RangeID: 10
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (8, 3, 2, 9, 5, 8, 35, 10);
+                
 
 -- For RangeID: 11
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (1, 10, 4, 10, 7, 7, 39, 11);
-
-
--- For RangeID: 11
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 10, 5, 8, 7, 3, 41, 11);
-
+VALUES (9, 8, 10, 9, 3, 1, 40, 11);
+                
 
 -- For RangeID: 11
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (8, 2, 1, 6, 6, 8, 31, 11);
-
+VALUES (8, 10, 3, 4, 9, 9, 43, 11);
+                
 
 -- For RangeID: 11
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 9, 8, 1, 9, 9, 46, 11);
+VALUES (9, 5, 6, 1, 8, 3, 32, 11);
+                
 
+-- For RangeID: 11
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (6, 10, 9, 5, 7, 1, 38, 11);
+                
+
+-- For RangeID: 11
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (3, 10, 5, 7, 1, 7, 33, 11);
+                
 
 -- For RangeID: 12
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (2, 3, 8, 2, 8, 5, 28, 12);
-
-
--- For RangeID: 12
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (7, 10, 6, 8, 2, 4, 37, 12);
-
+VALUES (7, 6, 10, 5, 2, 6, 36, 12);
+                
 
 -- For RangeID: 12
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (6, 10, 5, 10, 9, 9, 49, 12);
-
+VALUES (7, 9, 5, 3, 3, 4, 31, 12);
+                
 
 -- For RangeID: 12
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (4, 10, 2, 1, 6, 2, 25, 12);
+VALUES (8, 10, 7, 2, 9, 5, 41, 12);
+                
 
+-- For RangeID: 12
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (2, 10, 6, 6, 6, 6, 36, 12);
+                
+
+-- For RangeID: 12
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (9, 10, 7, 3, 5, 9, 43, 12);
+                
 
 -- For RangeID: 13
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (3, 6, 10, 4, 5, 5, 33, 13);
-
-
--- For RangeID: 13
-INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (9, 6, 4, 1, 10, 7, 37, 13);
-
+VALUES (3, 1, 7, 3, 5, 5, 24, 13);
+                
 
 -- For RangeID: 13
 INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
-VALUES (10, 10, 5, 2, 7, 5, 39, 13);
+VALUES (5, 2, 2, 10, 6, 6, 31, 13);
+                
 
+-- For RangeID: 13
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (1, 5, 3, 5, 7, 8, 29, 13);
+                
+
+-- For RangeID: 13
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (2, 8, 9, 6, 3, 6, 34, 13);
+                
+
+-- For RangeID: 13
+INSERT INTO EndScoreTable (Arrow1, Arrow2, Arrow3, Arrow4, Arrow5, Arrow6, TotalEndScore, RangeID)
+VALUES (3, 3, 9, 3, 5, 4, 27, 13);
+                
