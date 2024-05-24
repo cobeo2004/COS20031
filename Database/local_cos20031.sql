bcd8aae03100f3f898f628b59cd0d65ea39c75cb
cobeo2004 <minh030963@stu.vinschool.edu.vn>
2024-05-24 11:59:56 +1000

    Upload: Use directives

--- Database/local_cos20031.sql
+++ Database/local_cos20031.sql
@@ -15,6 +15,9 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+-- USE DIRECTIVES
+CREATE TABLE IF NOT EXISTS cos20031_5_db;
+USE cos20031_5_db;
 --
 -- Table structure for table `ArcherTable`
 --
